739cae5cad04c3530fd54c3dbcc8039283d91876
Yehor Malchenko <malchenko.yehor@gmail.com>
2021-03-25 20:35:42 +0100

    Added tests to paragraph

--- src/components/atoms/Paragraph/Paragraph.test.tsx
+++ src/components/atoms/Paragraph/Paragraph.test.tsx
@@ -1,5 +1,12 @@
+import { render } from '@testing-library/react';
 import React from 'react';
 import Paragraph from './Paragraph';
 
 
-describe('<Paragraph>', () => {});
+describe('<Paragraph>', () => {
+  test('renders text correctly', () => {
+    const { getByText } = render(<Paragraph text="test" />);
+
+    expect(getByText('test')).toBeInTheDocument();
+  });
+});
