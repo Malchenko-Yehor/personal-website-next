6edbd9b179a55cdfcf15047071c0eb97f6d65d1d
Yehor Malchenko <malchenko.yehor@gmail.com>
2021-04-02 10:57:45 +0200

    added circle icon atom

---
+++ src/components/atoms/CircleIcon/CircleIcon.stories.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import CircleIcon, { CircleIconProps } from "./CircleIcon";
+
+export default {
+  title: 'Atoms/CircleIcon',
+  component: CircleIcon
+};
+
+const Template = (args: CircleIconProps) => <CircleIcon {...args} />;
+
+export const Default = Template.bind({});
+
+Default.args = {
+  src: '/images/emakina.svg',
+} as CircleIconProps;
---
+++ src/components/atoms/CircleIcon/CircleIcon.styled.ts
@@ -0,0 +1,12 @@
+import { cOrange } from "@styles/variables";
+import styled from "styled-components";
+
+export const CirleIcon = styled.i`
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  width: 50px;
+  height: 50px;
+  background-color: ${cOrange};
+  border-radius: 50%;
+`;
